0616fd555864cdc75f4ee0cd2f014449b5effa5e
Cecile DeVera <ceciledevera@Ceciles-MacBook-Pro.local>
2019-10-09 20:24:46 -0700

    Add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
-
-
-$(document).ready(function() {
-$("#input-number").sumbit(function(event) {
-  event.preventDefault();
-   var inputtedNumber = parseInt($("input#number").val());
-   $("#return-numbers").append("<li>" + returnNumbers + "</li>");
-
- });
-});
+// business logic
+var pingPong = function(inputN) {
+  var result = [];
+  for (var i = 0; i <= inputN; i++ ) {
+    if (i % 3 === 0 && i % 15 !== 0) {
+      result.push("PING");
+    } else if (i % 5 === 0 && i % 15 !== 0) {
+      result.push("PONG");
+    } else if (i % 15 === 0) {
+      result.push("PING-PONG");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+};
